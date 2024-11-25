1528736236a011cf4e64e4f471e7b713d3a3965f
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2024-11-25 15:32:44 -0300

    Update controle.vhd
    Não sei oq falta, mas os estados tão feitos ali

--- controle.vhd
+++ controle.vhd
@@ -4,19 +4,6 @@ library ieee;
 use ieee.std_logic_1164.all;
 
 entity controle is
-port
-(
-BTN1, BTN0, clock_50: in std_logic;
-sw_erro, end_game, end_time, end_round: in std_logic;
-R1, R2, E1, E2, E3, E4, E5: out std_logic
-);
-end entity;
---Bloco de controle, tem a descrição de funcionamento da máquina de estados. Garante que tudo funciona como deve, saídas e transições de um estado para outro.
-
-library ieee;
-use ieee.std_logic_1164.all;
-
-entity controle is
 port(BTN1, BTN0, clock_50: in std_logic;
     sw_erro, end_game, end_time, end_round: in std_logic;
     R1, R2, E1, E2, E3, E4, E5: out std_logic
@@ -88,15 +75,5 @@ begin
                 E5 <= '0';
 
 
-
-end architecture;
-architecture arc of controle is
-	type State is (Start, Setup, Play, Count_Round, Check, Waits, Result); --Aqui temos os estados
-	signal EA, PE: State := Start; 						-- PE: proximo estado, EA: estado atual 
-
-begin
-
-
--- FSM usando dois process a ser feito pel@ alun@
 
 end architecture;
