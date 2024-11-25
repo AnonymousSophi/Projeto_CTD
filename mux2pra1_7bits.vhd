714e84098d725f2ce0f085be19f189ae6b568abb
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2024-11-25 16:50:53 -0300

    Update mux2pra1_7bits.vhd

--- mux2pra1_7bits.vhd
+++ mux2pra1_7bits.vhd
@@ -4,15 +4,14 @@ use IEEE.std_logic_arith.all;
 use IEEE.std_logic_unsigned.all;
 
 entity mux2pra1_7bits is port (
-    S     : in  std_logic;
-    L0, L1: in  std_logic_vector(6 downto 0);
-    D     : out std_logic_vector(6 downto 0));
+    sel    : in  std_logic;
+    x, y : in  std_logic_vector(6 downto 0);
+    saida     : out std_logic_vector(6 downto 0));
 end mux2pra1_7bits;
         
 architecture arqdtp of mux2pra1_7bits is
     begin
     
-     D <=  L0   when s <= '0' else
-		   L1;
-		   
+     saida <=  x   when sel <= '0' else
+		   y;	                        
 end arqdtp;
