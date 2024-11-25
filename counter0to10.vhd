819b355c398c2e131009cb476393812f6b902cdd
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2024-11-25 16:39:02 -0300

    Update counter0to10.vhd

--- counter0to10.vhd
+++ counter0to10.vhd
@@ -1,1 +1,14 @@
+library ieee;
+use ieee.std_logic_1164.all; 
+use ieee.std_logic_unsigned.all; 
+use ieee.std_logic_arith.all;
 
+
+entity counter0to10 is
+port(
+    Enable, Reset, CLOCK: in std_logic;
+	Round: out std_logic_vector(3 downto 0);
+	end_round: out std_logic
+	);
+			
+end counter0to10;
