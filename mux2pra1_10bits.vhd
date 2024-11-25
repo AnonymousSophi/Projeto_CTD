601852ebc26d899bcfb69db80c7672cf36f9eb11
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2024-11-25 16:48:55 -0300

    Create mux2pra1_10bits.vhd

---
+++ mux2pra1_10bits.vhd
@@ -0,0 +1,17 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use IEEE.std_logic_arith.all;
+use IEEE.std_logic_unsigned.all;
+
+entity mux2pra1_10bits is port (
+    sel    : in  std_logic;
+    x, y: in  std_logic_vector(9 downto 0);
+    saida     : out std_logic_vector(9 downto 0));
+end mux2pra1_10bits;
+        
+architecture arqdtp of mux2pra1_10bits is
+    begin
+    
+     saida <=  x   when sel <= '0' else
+		   y;	                        
+end arqdtp;
