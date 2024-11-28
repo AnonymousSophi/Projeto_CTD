4210334ba1df23c6c70457857b980936ee44ddf0
AnonymousSoph <149272712+AnonymousSophi@users.noreply.github.com>
2024-11-27 21:24:11 -0300

    Update datapath.vhd
    No PORT MAP:
    Falta a saída de "reg8bits";
    Faltam as entradas de "DEC7SEG_hex1" e "DEC7SEG_hex0";
    
    Arquitetura:
    Não sei pq temos "reg4bits", pois ele não é usado na imagem do DATAPATH em PDF.

--- datapath.vhd
+++ datapath.vhd
@@ -98,8 +98,8 @@ end component;
 
 component mux2pra1_7bits is
 port (sel: in std_logic;
-      x, y: in std_logic_vector(6 downto 0);
-      saida: out std_logic_vector(6 downto 0)
+		x, y: in std_logic_vector(6 downto 0);
+		saida: out std_logic_vector(6 downto 0)
 );
 end component;
 
@@ -158,13 +158,18 @@ end component;
 
 signal selMux23, selMux45, end_game_interno, end_round_interno, clk_1, enableRegFinal: std_logic; --1 bit
 signal Round, Level_time, Level_code, SaidaCountT, SomaDigitada, SomaSelDig, CounterTMux: std_logic_vector(3 downto 0); -- 4 bits
-signal decMuxCode, decMuxRound, muxMux2, muxMux3, decMux4, Tempo, t, r, n: std_logic_vector(6 downto 0); -- 7 bits
-signal SomaSelDig_estendido,SeqLevel, RegFinal, valorfin_vector, MuxSelDig: std_logic_vector(7 downto 0); -- 8 bits
+signal decMuxCode, decMuxRound, muxMux2, muxMux3, decMux4, Tempo, t, r, n, muxHEX5, muxHEX4, muxHEX3, muxHEX2, decHEX1, decHEX0: std_logic_vector(6 downto 0); -- 7 bits
+signal SomaSelDig_estendido, SeqLevel, RegFinal, valorfin_vector, MuxSelDig: std_logic_vector(7 downto 0); -- 8 bits
 signal N_unsigned: unsigned(3 downto 0);
 signal SeqDigitada, ComparaSelDig, SelecionadaROM, EntradaLEDS: std_logic_vector(9 downto 0); -- 10 bits
 
 begin
 
+            ----------------------------------------
+                ---- TODOS OS PORT MAP ----
+            ----------------------------------------
+
+------------------------DIVISOR FREQ------------------------------
 
 DIV: Div_Freq port map (CLOCK_50, R2, clk_1); -- Para teste no emulador, comentar essa linha e usar o CLK_1Hz
 
@@ -172,27 +177,60 @@ DIV: Div_Freq port map (CLOCK_50, R2, clk_1); -- Para teste no emulador, comenta
 
 COUNTERTIME: counter_time port map (E2, R1, clk_1, end_time, tempo);
 
-CONTER0TO10: counter0to10 port map (E3, R2, clk_1, Round, end_round,);
+COUNTER_0_TO_10: counter0to10 port map (E3, R2, clk_1, Round, end_round);
 
 -------------------ELEMENTOS DE MEMORIA-------------------------
 
---REG4BITS: reg4bits port map (CLOCK_50, );-- ??????
-REG8BITS: reg8bit port map (CLOCK_50, R2, E1, SW(7 downto 0), ); -- como faz a saída desses?
-REG10BIT: reg10bits port map (CLOCK_50, R2, E2, SW(9 downto 0), ); -- '''
-ROM0: ROM port map (Level_code, CODE);
+--REG4BITS: reg4bits port map (CLOCK_50, R2, );     			    -------------- ???
+REG8BITS_1: reg8bits port map (CLOCK_50, R2, E1, SW(7 downto 0), );         ------------- FALTA A SAÍDA!!
+REG8BITS_2: reg8bits port map (CLOCK_50, R2, (E5 or E4), MuxSelDig, valorfin_vector);
+REG10BITS: reg10bits port map (CLOCK_50, R2, E2, SW(9 downto 0), ComparaSelDig); 
+ROM0: ROM port map (Level_code, SelecionadaROM);
 
 ---------------------MULTIPLEXADORES----------------------------
 
--- a fazer pel@ alun@
+-- 4bits
+
+MUX_2X1_4BITS: mux2pra1_4bits port map(E2, Tempo, Level_time, CounterTMux); -- saída (CounterTMux) entra no decod7seg (para o display decodificador)
+
+-- 7bits
+
+MUX_2x1_T: mux2pra1_7bits port map ((E1 or E2), t, "1111111", muxHEX5); -- letra 't' p/ display, (HEX5)
+
+MUX_2x1_: mux2pra1_7bits port map ((R1 xor R2), decMux4, "1111111", muxHEX4); -- (HEX4)
+
+MUX_2x1_R: mux2pra1_7bits port map ((R1 xor R2), r, muxMux3, muxHEX3);-- (HEX3)
+MUX_2x1_N: mux2pra1_7bits port map (E1, n, "1111111", muxMux3);  -- (HEX3)AUXILIAR
+
+MUX_2x1_ROUND: mux2pra1_7bits port map ((R1 xor R2), decMuxRound, muxMux2, muxHEX2); -- (HEX2)
+MUX_2x1_LEVELCODE: mux2pra1_7bits port map (E1, decMuxCode, "1111111", muxMux2);   -- (HEX2)AUXILIAR
+
+-- 8bits
+
+MUX_2x1_8BITS: mux2pra1_8bits port map (E5, "000" & end_game & Round, "1010" & SomaDigitada, MuxSelDig);
+
+-- 10bits
+
+MUX_2x1_10BITS: mux2pra1_10bits port map (E5, "0000000000", SelecionadaROM, EntradaLEDS);
 
 -------------------COMPARADORES E SOMA--------------------------
 
--- a fazer pel@ alun@
+COMP_SOMA: comp port map (SelecionadaROM, ComparaSelDig, SeqDigitada);
+
+COMP_IGUAL_4_erro: comp_igual4 port map (SomaSelDig, sw_erro);
+COMP_IGUAL_4_endgame: comp_igual4 port map (SomaDigitada, end_game);
+
+SOMA_seq: soma port map (SeqDigitada, SomaDigitada);
+SOMA_seldig: soma port map (ComparaSelDig, SomaSelDig);
         
 ---------------------DECODIFICADORES----------------------------
 
--- a fazer pel@ alun@
-        
+DEC7SEG_hex4: decod7seg port map (CounterTMux, decMux4);
+DEC7SEG_hex2_1: decod7seg port map (Round, decMuxRound);
+DEC7SEG_hex2_2: decod7seg port map (Level_code, decMuxCode);
+DEC7SEG_hex1: decod7seg port map (, decHEX1);            ------------- FALTAM AS
+DEC7SEG_hex0: decod7seg port map (, decHEX0);            ------------- ENTRADAS!!
+
 ---------------------ATRIBUICOES DIRETAS---------------------
 
 -- a fazer pel@ alun@
